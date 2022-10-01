b110594c62ce79875fd45fa0c5c6fdf1dbc2521e
Kimberly Villatoro <kimsvillatoro@gmail.com>
2022-10-01 12:55:35 -0400

    Change color pallete, about page complete

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import './index.scss';
+
+const About = () => {
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    About Me
+                </h1>
+            </div>
+            <div className='text-zone'>
+                <p>I am a female in STEM who is passionate about creating change.  Recent graduate at the University of Maryland Global Campus with a B.S. in Computer Science.</p>
+                <p>
+                I have a strong desire to learn new skills and have the work ethic and determination to continuously develop them in a changing environment.
+                </p>
+                <p>The roles I am interested in going into are software development, web development, and android development.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,10 +3,12 @@ import LogoTitle from '../../assets/images/logok.png'
 import './index.scss';
 
 const Home = () => {
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1> Hi, <br /> I'm
+                <h1>
+                    Hi, <br /> I'm
                 <img src = {LogoTitle} alt="developer" />
                 <br /> full stack engineer.
                 </h1>
